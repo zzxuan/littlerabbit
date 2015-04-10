562891c609a2bfc0224dc039261574074b1289ea
zzxuan <xiaoxuanfengzzx@163.com>
2015-04-10 18:16:46 +0800

    Signed-off-by: zzxuan <xiaoxuanfengzzx@163.com>

---
+++ src/littlerabbit/SysMsg/CpuMsg.cpp
@@ -0,0 +1,67 @@
+#include "CpuMsg.h"
+#include "stdafx.h"
+#include "windows.h" 
+#include "iostream"
+#include "string"
+
+using namespace std;
+//用来存储信息
+DWORD deax;
+DWORD debx;
+DWORD decx;
+DWORD dedx;
+
+void ExeCPUID(DWORD veax)//初始化CPU
+{
+	__asm
+	{
+		mov eax,veax
+			cpuid
+			mov deax,eax
+			mov debx,ebx
+			mov decx,ecx
+			mov dedx,edx
+	}
+}
+
+long GetCPUFreq()//获取CPU频率,单位: MHZ
+{
+	int start1,start2;
+	_asm rdtsc
+	_asm mov start1,eax
+	Sleep(50);
+	_asm rdtsc
+	_asm mov start2,eax
+	return (start2-start1)/50000;
+}
+
+void GetManID(char *ID,int len)//获取制造商信息
+{
+	//char ID[25];//存储制造商信息
+	memset(ID,0,len);//先清空数组 ID
+	ExeCPUID(0);//初始化
+	memcpy(ID+0,&debx,4);//制造商信息前四个字符复制到数组
+	memcpy(ID+4,&dedx,4);//中间四个
+	memcpy(ID+8,&decx,4);//最后四个
+	//如果返回 char * ,会出现乱码;故返回 string 值
+	return;
+}
+
+void GetCpuTypeMsg(char *CPUType,int len)
+{
+	const DWORD id = 0x80000002; //从0x80000002开始,到0x80000004结束
+	//char CPUType[49];//用来存储CPU型号信息
+	memset(CPUType,0,len);//初始化数组
+
+	for(DWORD t = 0 ; t < 3 ; t++ )
+	{
+		ExeCPUID(id+t);
+		//每次循环结束,保存信息到数组
+		memcpy(CPUType+16*t+ 0,&deax,4);
+		memcpy(CPUType+16*t+ 4,&debx,4);
+		memcpy(CPUType+16*t+ 8,&decx,4);
+		memcpy(CPUType+16*t+12,&dedx,4);
+	}
+
+	return;
+}
--- src/littlerabbit/SysMsg/GetMsgInfo.cpp
+++ src/littlerabbit/SysMsg/GetMsgInfo.cpp
@@ -3,45 +3,78 @@
 #include <stdio.h>
 #include <windows.h>
 #include <iostream>
+#include "CpuMsg.h"
 
 using namespace std;
 
+void SetPCMsg(PCMsg *msg,int id,char* tpye,char* name,char* val)
+{
+	msg->msgID = id;
+	sprintf(msg->msgName,name);
+	sprintf(msg->msgType,tpye);
+	sprintf(msg->msgVal,val);
+}
+
 GetMsgInfo::GetMsgInfo(void)
 {
-	printf("GetMsgInfo\n");
+	
 }
 
 
 GetMsgInfo::~GetMsgInfo(void)
 {
-	printf("~GetMsgInfo\n");
+	
 }
 
-void GetMsgInfo::Test()
+void GetMsgInfo::GetComputerMsg()
 {
-	printf("test\n");
+	msgVec.clear();
 	GetCpuType();
+	GetMemMsg();
 }
 
 void GetMsgInfo::GetCpuType()
 {
-	//获得CPU型号
-    SYSTEM_INFO systemInfo;
-	GetSystemInfo (&systemInfo);
-	cout << "处理器掩码: " << systemInfo.dwActiveProcessorMask << endl
-		 << "处理器个数: " << systemInfo.dwNumberOfProcessors << endl
-		 << "处理器分页大小: " << systemInfo.dwPageSize << endl
-		 << "处理器类型: " << systemInfo.dwProcessorType << endl
-		 << "最大寻址单元: " << systemInfo.lpMaximumApplicationAddress << endl
-		 << "最小寻址单元: " << systemInfo.lpMinimumApplicationAddress << endl
-		 << "处理器等级: " << systemInfo.wProcessorLevel << endl
-		 << "处理器版本: " << systemInfo.wProcessorRevision << endl;
-	cout << "---------------------------------------------------"<<endl;
+	{
+		PCMsg msg = {"","","",-1};
+		char cc1[20];
+		sprintf(cc1,"%d",GetCPUFreq());
+		SetPCMsg(&msg,1,"CPU","CPU 主 频",cc1);
+		msgVec.push_back(msg);
+	}
+	{
+		PCMsg msg = {"","","",-1};
+		char cc[50];
+		GetManID(cc,sizeof(cc));
+		SetPCMsg(&msg,2,"CPU","CPU 制造商",cc);
+		msgVec.push_back(msg);
+	}
+	{
+		PCMsg msg = {"","","",-1};
+		char cc[50];
+		GetCpuTypeMsg(cc,sizeof(cc));
+		SetPCMsg(&msg,3,"CPU","CPU 型 号",cc);
+		msgVec.push_back(msg);
+	}
+}
 
+void GetMsgInfo::GetMemMsg()
+{
 	MEMORYSTATUS memory;
 	memory.dwLength =sizeof(memory); //初始化
 	GlobalMemoryStatus(&memory);
-	cout << "您的物理内存是(Mb):"<<(int)(memory.dwTotalPhys /1024/1024)<<endl;
-	cout << "其中可用内存是(Kb):"<<(int)( memory.dwAvailPhys /1024)<<endl;
-
+	{
+		PCMsg msg = {"","","",-1};
+		char cc1[20];
+		sprintf(cc1,"%d",(int)(memory.dwTotalPhys /1024/1024));
+		SetPCMsg(&msg,4,"Mem","物理内存是(Mb)",cc1);
+		msgVec.push_back(msg);
+	}
+	{
+		PCMsg msg = {"","","",-1};
+		char cc1[20];
+		sprintf(cc1,"%d",(int)(memory.dwAvailPhys /1024/1024));
+		SetPCMsg(&msg,5,"Mem","可用内存是(Mb)",cc1);
+		msgVec.push_back(msg);
+	}
 }
